fb72ac466c1a73d4df2858585dd9eceb44ce36b1
Danil Sch <73740737+BreakingTV@users.noreply.github.com>
2021-11-09 19:09:34 +0100

    Added a get request

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.set('views', __dirname + '/views');
+app.get('/', (req, res) => res.render('index.pug'))
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`=============================`)
