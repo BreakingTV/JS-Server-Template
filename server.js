48754c9a963d04e331fa6cf6c5e6a0065fafbd10
Danil Sch <73740737+BreakingTV@users.noreply.github.com>
2021-11-18 21:31:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(`${__dirname}/assets`));
+app.locals.basedir = `${__dirname}/assets`;
+
 app.set('views', __dirname + '/views');
 app.get('/', (req, res) => res.render('index.pug'))
 
